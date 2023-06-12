87629c3b22ec76feb3bba1ba39bde5f954153b77
Bedjo <bedzovski@yahoo.com>
2023-06-12 23:45:43 +0200

    woo

--- upwork_project_one/settings.py
+++ upwork_project_one/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(",")
 
 
 # Application definition
